c155b6bfa8abaa23bac05dcaad9f40027d88dff9
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-07-16 16:31:53 +0300

    made footer modul + some dir structure

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+import heart from '../../assets/images/heart.svg'
+
+const Footer = () => (
+    <footer className="footer">
+      <span className="footer__text">
+        from <a className="footer__link" href="https://binary-studio.com">binary studio</a> with
+        <img className="footer__icon" src={heart} alt="heart icon" />
+      </span>
+    </footer>
+);
+
+export default Footer;
